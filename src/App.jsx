b8f603a9c210647e140af97631f5eae9a7059159
ritesh <riteshkumar.dev2@gmail.com>
2025-12-03 13:09:07 +0530

    Update project in angular

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Layout from './layout/Layout'
-import { TASKS_DATA } from './data'
-function App() {
-
-  return <Layout initialTasks={TASKS_DATA} />
-}
-
-export default App
---
+++ src/app/layout/main-body/main-body.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Dashboard } from '../../pages/dashboard/dashboard';
+
+@Component({
+  selector: 'app-main-body',
+  imports: [Dashboard],
+  templateUrl: './main-body.html',
+  styleUrl: './main-body.css',
+})
+export class MainBody {
+
+}
