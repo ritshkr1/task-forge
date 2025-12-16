54a4a3ff0e1e96e052ad60a18fb7dc403b448058
riteshkrdev <riteshkumar.dev2@gmail.com>
2025-12-16 17:55:34 +0530

    add tailwind and material ui deps

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,31 +6,33 @@ export default function Header({ tabName, setTabName, setModalOpen }) {
         border: 'none',
         marginLeft: '5px',
     }
-    return <header>
+    return (
+      <header>
         <span>
-            Task Forge
-            <button
-                className={tabName === 'Dashboard' ? 'tab-button-active' : 'tab-button'}
-                onClick={() => setTabName("Dashboard")}
-                
-            >
-                Dashboard
-            </button>
-            <button
-                className={tabName === 'Board' ? 'tab-button-active' : 'tab-button'}
-                onClick={() => setTabName("Board")}
-                
-            >
-                Board
-            </button>
-            <button
-                className={tabName === 'Table' ? 'tab-button-active' : 'tab-button'}
-                onClick={() => setTabName("Table")}
-                
-            >
-                Table
-            </button>
+          <span class="text-xl font-bold">Task Forge</span>
+
+          <button
+            className={
+              tabName === "Dashboard" ? "tab-button-active" : "tab-button"
+            }
+            onClick={() => setTabName("Dashboard")}
+          >
+            Dashboard
+          </button>
+          <button
+            className={tabName === "Board" ? "tab-button-active" : "tab-button"}
+            onClick={() => setTabName("Board")}
+          >
+            Board
+          </button>
+          <button
+            className={tabName === "Table" ? "tab-button-active" : "tab-button"}
+            onClick={() => setTabName("Table")}
+          >
+            Table
+          </button>
         </span>
-        <button onClick={() => setModalOpen('add')} >Add Task</button>
-    </header>
+        <button onClick={() => setModalOpen("add")}>Add Task</button>
+      </header>
+    );
 }
